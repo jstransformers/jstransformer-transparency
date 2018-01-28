70bd6230be1bfbfa66cb8690e101a00f49f36b59
Rob Loach <robloach@gmail.com>
2018-01-28 16:23:15 -0500

    1.0.0

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## v1.0.0: 2018-01-28
+
+- Updated Boilerplate
+- Updated dependencies
+
 ## v0.1.0: 2016-12-18
 
 - Updated Boilerplate
--- index.js
+++ index.js
@@ -4,20 +4,15 @@ const transparency = require('transparency')
 const jsdom = require('jsdom')
 const extend = require('extend-shallow')
 
+const {JSDOM} = jsdom
+
 exports.name = 'transparency'
 exports.outputFormat = 'html'
 
-exports.compileAsync = function (str, options) {
-  return new Promise(function (resolve, reject) {
-    jsdom.env(str, function (err, window) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(function (locals) {
-          const out = transparency.render(window.document.body, extend({}, options, locals))
-          return out.innerHTML
-        })
-      }
-    })
-  })
+exports.compile = function (str, options) {
+  const windowdom = (new JSDOM(str)).window
+  return locals => {
+    const out = transparency.render(windowdom.document.body, extend({}, options, locals))
+    return out.innerHTML
+  }
 }
